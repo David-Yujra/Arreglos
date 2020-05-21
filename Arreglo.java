66d2501f4c2deeba6597e54b0e19ee9af6d4a112
David Yujra <att.mili_darco@hotmail.es>
2020-05-21 08:46:06 -0400

    Primer commit Fix

--- Arreglo.java
+++ Arreglo.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
  
 public class TestArreglos {
+//Se creo un progrma de Arreglos
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		float promedio = 0;
